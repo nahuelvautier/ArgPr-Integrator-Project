052778f99c2b6b29432a67ecbf40911bafd9fac0
Mr. Robot <nahuelearning@gmail.com>
2022-07-26 01:28:55 -0300

    First commit adding header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppLogoComponent } from './components/app-logo/app-logo.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AppLogoComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule
