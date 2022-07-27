c9962026b6e049fa96aa9b3379bc5f195593033c
Mr. Robot <nahuelearning@gmail.com>
2022-07-26 23:34:47 -0300

    creating about component and modifing pertinent styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppLogoComponent } from './components/app-logo/app-logo.component';
 import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProfilePhotoComponent } from './components/profile-photo/profile-photo.component';
+import { AboutDescriptionComponent } from './components/about-description/about-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AppLogoComponent,
-    SocialMediaComponent
+    SocialMediaComponent,
+    BannerComponent,
+    AboutComponent,
+    ProfilePhotoComponent,
+    AboutDescriptionComponent
   ],
   imports: [
     BrowserModule
