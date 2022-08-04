101ba4e5fd250c24179bf878a1b5d473c4861bc3
Mr. Robot <nahuelearning@gmail.com>
2022-08-04 05:29:57 -0300

    back-end dist added with db config without connecting tests for time issues

---
+++ back-end/src/main/java/com/portfolio/nahuelvautier/NahuelvautierApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.nahuelvautier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NahuelvautierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NahuelvautierApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/portfolio/nahuelvautier/NahuelvautierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.nahuelvautier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NahuelvautierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
