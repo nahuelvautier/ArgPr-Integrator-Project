8ef934835185285d69858ccbaec4ea83daa68017
nahuelvautier <nahuelearning@gmail.com>
2022-08-10 22:49:30 -0300

    Authorizations for ADMIN_ROLE added to persons path

--- back-end/src/main/java/com/portfolio/nahuelvautier/Controller/PersonController.java
+++ back-end/src/main/java/com/portfolio/nahuelvautier/Controller/PersonController.java
@@ -4,6 +4,7 @@ import com.portfolio.nahuelvautier.Entity.Person;
 import com.portfolio.nahuelvautier.Interface.IPersonService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,18 +25,21 @@ public class PersonController {
         return ipersonService.getPerson();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("persons/create")
     public String createPerson(@RequestBody Person person) {
         ipersonService.savePerson(person);
         return "Usuario creado correctamente.";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("persons/delete/{id}")
     public String deletePerson(@PathVariable Long id) {
         ipersonService.deletePerson(id);
         return "Usuario eliminado correctamente.";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("persons/edit/{id}")
     public Person editPerson(
             @PathVariable Long id,
